dd5694f94cf4e8bf3a9673f324f5c06720a89b86
Agustin Gargaglione <agusgarga@hotmail.com>
2017-08-18 12:00:55 -0300

    Arreglados los porcentajes

--- CarSolutioner/Forms/Menus/Admin/Partes/Clientes.vb
+++ CarSolutioner/Forms/Menus/Admin/Partes/Clientes.vb
@@ -9,7 +9,7 @@ Partial Public Class MainMenu
     Private Sub FiltrosCliente(sender As Object, e As EventArgs) Handles txtDocumFClientes.TextChanged, txtNombreFClientes.TextChanged, txtApellidoFClientes.TextChanged, txtCorreoFClientes.TextChanged, txtEmpresaFClientes.TextChanged, cbxTipoDocumFCliente.SelectionChangeCommitted
 
         Dim filtro As String
-        filtro = String.Format("{0} LIKE '{1}%' AND {2} LIKE '{3}%' AND {4} LIKE '{5}%' AND {6} LIKE '{7}%' AND {8} LIKE '{9}%' AND {10} LIKE '{11}%'",
+        filtro = String.Format("{0} LIKE '%{1}%' AND {2} LIKE '%{3}%' AND {4} LIKE '%{5}%' AND {6} LIKE '%{7}%' AND {8} LIKE '%{9}%' AND {10} LIKE '%{11}%'",
                                "nrodocumento", txtDocumFClientes.Text, "tipodocumento", cbxTipoDocumFCliente.SelectedItem.ToString, "nombre", txtNombreFClientes.Text, "apellido", txtApellidoFClientes.Text, "email", txtCorreoFClientes.Text, "empresa", txtEmpresaFClientes.Text)
 
         dgvClientes.DataSource.Filter = filtro
